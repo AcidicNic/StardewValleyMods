94d9c388209696b1067425d6d22ad84291a9f3cb
AcidicNic <nicole.r.abadie@gmail.com>
2024-03-28 19:20:24 -0700

    Renamed SpecialOrdersAnywhere -> MenuCycle. Changed some logic, made the code more readable, and changed the structure of the project. It still functions the same as before.

---
+++ MenuCycle/Framework/ModConfig.cs
@@ -0,0 +1,21 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Utilities;
+
+namespace AcidicNic.MenuCycle.Framework
+{
+    public class ModConfig
+    {
+        public KeybindList ActivateKey { get; set; } = new KeybindList(SButton.P);
+        public KeybindList CycleLeftKey { get; set; } = new KeybindList(SButton.OemOpenBrackets);
+        public KeybindList CycleRightKey { get; set; } = new KeybindList(SButton.OemCloseBrackets);
+
+        public bool EnableCalendar { get; set; } = true;
+        public bool EnableDailyQuests { get; set; } = true;
+        public bool EnableSpecialOrders { get; set; } = true;
+        public bool EnableQiSpecialOrders { get; set; } = true;
+        public bool EnableJournal { get; set; } = false;
+
+        public bool SpecialOrdersBeforeUnlocked { get; set; } = false;
+        public bool QiBeforeUnlocked { get; set; } = false;
+    }
+}
